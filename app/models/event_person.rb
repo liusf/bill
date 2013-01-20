5459d336fb96bb18e00869eb66a99d4e61ddc992
liusf <lsf.weald@gmail.com>
2013-01-21 01:36:42 +0800

    add capistrano

--- app/models/event_person.rb
+++
@@ -1,4 +0,0 @@
-class EventPerson < ActiveRecord::Base
-	belongs_to :event
-	belongs_to :person
-end
--- app/models/person.rb
+++
@@ -1,8 +0,0 @@
-class Person < ActiveRecord::Base
-	validates_uniqueness_of :short
-	has_many :memberships
-	has_many :groups, :through => :memberships
-
-	has_many :event_people
-	has_many :events, :through => :event_people
-end
